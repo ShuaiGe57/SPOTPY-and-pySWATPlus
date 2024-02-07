67568013f9390ce2bcc65efd6f4169f54308fc90
ShuaiGe57 <46555076+ShuaiGe57@users.noreply.github.com>
2024-02-08 00:55:58 +0800

    modify

--- SA.py
+++ SA.py
@@ -14,7 +14,8 @@ from SALib.analyze import morris as analyze
 from functools import partial
 from tqdm import tqdm
 
-def parallel_swat(params_list):
+
+def swat(params_list):
     # 这里是不是只运行一组参数就行
     cwd, reader, swat_params, tpl_params, copy_path, show_output, delete_copy = params_list
     result = reader.copy_and_run(dir=copy_path,
@@ -44,7 +45,7 @@ def parallel_swat(params_list):
 
 if __name__ == "__main__":
     problem = {
-        "num_vars": 46,
+        "num_vars": 68,
         "names": [
             "lat_ttime",
             "can_max",
@@ -61,12 +62,14 @@ if __name__ == "__main__":
             "gw_flo",
             "dep_bot",
             "dep_wt",
+            "no3_n",
             "flo_dist",
             "bf_max",
             "alpha_bf",
             "revap",
             "rchg_dp",
             "spec_yld",
+            "hl_no3n",
             "flo_min",
             "revap_min",
             "fall_tmp",
@@ -75,6 +78,7 @@ if __name__ == "__main__":
             "melt_lag",
             "snow_h2o",
             "cov50",
+            "snow_init",
             "dp",
             "t_fc",
             "lag",
@@ -86,14 +90,31 @@ if __name__ == "__main__":
             "lai_noevap",
             "sw_init",
             "surq_lag",
+            "orgn_min",
+            "n_uptake",
+            "n_perc",
+            "rsd_decomp",
             "msk_co1",
             "msk_co2",
             "msk_x",
+            "nperco_lchtile",
             "evap_adj",
             "scoef",
+            "denit_exp",
+            "denit_frac",
+            "n_fix_max",
+            "rsd_decay",
+            "rsd_cover",
             "surq_exp",
+            "exp_co",
+            "nitrate",
+            "fr_hum_act",
+            "hum_c_n",
+            "ovn",
+            "awc",
+            "soil_k",
+            "rsd_init",
             "fert",
-            "awc"
         ],
         "bounds": [
             [0.51, 179.0],
@@ -111,20 +132,23 @@ if __name__ == "__main__":
             [0.0, 2.0],
             [5.0, 50.0],
             [2.0, 20.0],
+            [0.1, 999.9],
             [10.0, 200.0],
             [0.11, 1.99],
             [0.01, 0.99],
-            [0.021, 0.199],
-            [0.001, 0.100],
-            [0.01, 0.49],
+            [0.021, 0.199],  # revap
+            [0.01, 0.99],
+            [0.01, 0.49],  # spec_yld
+            [0.1, 199.9],
             [0.1, 49.0],
             [0.1, 49.0],
             [-4.9, 4.9],
             [-4.9, 4.9],
-            [0.1, 9.9],
+            [0.1, 9.9],  # melt_max_min
             [0.01, 0.99],
             [0.1, 499.9],
             [0.01, 0.90],
+            [0, 1000],  # snow_init
             [0.1, 5999.9],
             [0.1, 99.9],
             [0.1, 99.9],
@@ -136,20 +160,37 @@ if __name__ == "__main__":
             [0.1, 9.9],
             [0.01, 0.99],
             [1.1, 23.9],
+            [0.0011, 0.0029],  # orgn_min
+            [0.1, 99.9],
+            [0.01, 0.99],
+            [0.021, 0.099],
             [0.1, 9.9],
             [0.1, 9.9],
             [0.01, 0.29],
+            [0.01, 0.99],  # nperco_lchtile
             [0.51, 0.99],
             [0.01, 0.99],
+            [0.01, 2.99],  # denit_exp
+            [0.01, 0.99],
+            [1.1, 19.9],
+            [0.001, 0.049],
+            [0.11, 0.49],
             [1.1, 2.9],
-            [10, 100],
-            [0.01, 0.9]
+            [0.01, 0.99],  # exp_co
+            [0.1, 99.9],
+            [0.01, 0.99],
+            [8.1, 11.9],
+            [0.011, 0.69],  # ovn_mean, ovn_min, ovn_max
+            [0.01, 0.99],
+            [0.1, 1999],  # soil_k
+            [1, 9999],
+            [1, 1000]
         ]
     }
 
     par = sample.sample(problem,
-                        N=2,
-                        num_levels=2,
+                        N=6,
+                        num_levels=6,
                         seed=1,
                         )
     np.savetxt("SA_X.txt", par, fmt="%.4f")
@@ -202,57 +243,83 @@ if __name__ == "__main__":
                        "aquifer.aqu": ("name", [(None, "gw_flo", par[i, 12]),
                                                 (None, "dep_bot", par[i, 13]),
                                                 (None, "dep_wt", par[i, 14]),
-                                                (None, "flo_dist", par[i, 15]),
-                                                (None, "bf_max", par[i, 16]),
-                                                (None, "alpha_bf", par[i, 17]),
-                                                (None, "revap", par[i, 18]),
-                                                (None, "rchg_dp", par[i, 19]),
-                                                (None, "spec_yld", par[i, 20]),
-                                                (None, "flo_min", par[i, 21]),
-                                                (None, "revap_min", par[i, 22]),
+                                                (None, "no3_n", par[i, 15]),
+                                                (None, "flo_dist", par[i, 16]),
+                                                (None, "bf_max", par[i, 17]),
+                                                (None, "alpha_bf", par[i, 18]),
+                                                (None, "revap", par[i, 19]),
+                                                (None, "rchg_dp", par[i, 20]),
+                                                (None, "spec_yld", par[i, 21]),
+                                                (None, "hl_no3n", par[i, 22]),
+                                                (None, "flo_min", par[i, 23]),
+                                                (None, "revap_min", par[i, 24]),
                                                 ],
                                        ),
-                       "snow.sno": ("name", [(None, "fall_tmp", par[i, 23]),
-                                             (None, "melt_tmp", par[i, 24]),
-                                             (None, "melt_max", par[i, 25]),
-                                             (None, "melt_min", par[i, 25]),
-                                             (None, "tmp_lag", par[i, 26]),
-                                             (None, "snow_h2o", par[i, 27]),
-                                             (None, "cov50", par[i, 28]),
+                       "snow.sno": ("name", [(None, "fall_tmp", par[i, 25]),
+                                             (None, "melt_tmp", par[i, 26]),
+                                             (None, "melt_max", par[i, 27]),
+                                             (None, "melt_min", par[i, 27]),
+                                             (None, "tmp_lag", par[i, 28]),
+                                             (None, "snow_h2o", par[i, 29]),
+                                             (None, "cov50", par[i, 30]),
+                                             (None, "snow_init", par[i, 31])
                                              ],
                                     ),
-                       "tiledrain.str": ("name", [(None, "dp", par[i, 29]),
-                                                  (None, "t_fc", par[i, 30]),
-                                                  (None, "lag", par[i, 31]),
-                                                  (None, "rad", par[i, 32]),
-                                                  (None, "dist", par[i, 33]),
-                                                  (None, "drain", par[i, 34]),
-                                                  (None, "pump", par[i, 35]),
-                                                  (None, "lat_ksat", par[i, 36]),
+                       "tiledrain.str": ("name", [(None, "dp", par[i, 32]),
+                                                  (None, "t_fc", par[i, 33]),
+                                                  (None, "lag", par[i, 34]),
+                                                  (None, "rad", par[i, 35]),
+                                                  (None, "dist", par[i, 36]),
+                                                  (None, "drain", par[i, 37]),
+                                                  (None, "pump", par[i, 38]),
+                                                  (None, "lat_ksat", par[i, 39]),
                                                   ],
                                          ),
-                       "parameters.bsn": ("igen", [(None, "lai_noevap", par[i, 37]),
-                                                   (None, "sw_init", par[i, 38]),
-                                                   (None, "surq_lag", par[i, 39]),
-                                                   (None, "msk_co1", par[i, 40]),
-                                                   (None, "msk_co2", par[i, 41]),
-                                                   (None, "msk_x", par[i, 42]),
-                                                   (None, "evap_adj", par[i, 43]),
-                                                   (None, "scoef", par[i, 44]),
-                                                   (None, "surq_exp", par[i, 45]),
+                       "parameters.bsn": ("igen", [(None, "lai_noevap", par[i, 40]),
+                                                   (None, "sw_init", par[i, 41]),
+                                                   (None, "surq_lag", par[i, 42]),
+                                                   (None, "orgn_min", par[i, 43]),
+                                                   (None, "n_uptake", par[i, 44]),
+                                                   (None, "n_perc", par[i, 45]),
+                                                   (None, "rsd_decomp", par[i, 46]),
+                                                   (None, "msk_co1", par[i, 47]),
+                                                   (None, "msk_co2", par[i, 48]),
+                                                   (None, "msk_x", par[i, 49]),
+                                                   (None, "nperco_lchtile", par[i, 50]),
+                                                   (None, "evap_adj", par[i, 51]),
+                                                   (None, "scoef", par[i, 52]),
+                                                   (None, "denit_exp", par[i, 53]),
+                                                   (None, "denit_frac", par[i, 54]),
+                                                   (None, "n_fix_max", par[i, 55]),
+                                                   (None, "rsd_decay", par[i, 56]),
+                                                   (None, "rsd_cover", par[i, 57]),
+                                                   (None, "surq_exp", par[i, 58]),
                                                    ],
                                           ),
+                       "nutrients.sol": ("name", [(None, "exp_co", par[i, 59]),
+                                                  (None, "nitrate", par[i, 60]),
+                                                  (None, "fr_hum_act", par[i, 61]),
+                                                  (None, "hum_c_n", par[i, 62]),
+                                                  ],
+                                         ),
+                       "ovn_table.lum": ("name", [(None, "ovn_mean", par[i, 63]),
+                                                  (None, "ovn_min", par[i, 63]),
+                                                  (None, "ovn_max", par[i, 63]),
+                                                  ],
+                                         ),
                        }
-        tpl_params = {"soils.sol.tpl": {"awc": par[i, 46]},
-                      "lum.dtl.tpl": {"fert": par[i, 47]}
+        tpl_params = {"soils.sol.tpl": {"awc": par[i, 64],
+                                        "soil_k": par[i, 65],
+                                        },
+                      "plant.ini.tpl": {"rsd_init": par[i, 66]},
+                      "lum.dtl.tpl": {"fert": par[i, 67]}
                       }
         params_list.append((cwd, reader, swat_params, tpl_params, copy_path, show_output, delete_copy))
-
 
     print("===================   Start   ===================")
     p = multiprocessing.Pool()
     # 第二个参数为alist
-    result = list(tqdm(p.imap(parallel_swat, params_list), total=num_runs))
+    result = list(tqdm(p.imap(swat, params_list), total=num_runs))
     p.close()
     p.join()
     np.savetxt("SA_Y.txt", result, fmt="%.4f")
---
+++ SA_TxtInOut/plant.ini.tpl
@@ -0,0 +1,18 @@
+plant.ini: written by SWAT+ editor v2.3.3 on 2024-01-30 23:01 for SWAT+ rev.60.5.7
+pcom_name          plt_cnt  rot_yr_ini          plt_name     lc_status      lai_init       bm_init      phu_init      plnt_pop      yrs_init      rsd_init  
+crgr_comm                1         1  
+                                        crgr             n       0.00000       0.00000       0.00000       0.00000       0.00000   #rsd_init#  
+frsd_comm                1         1  
+                                        frsd             y       2.00000   50000.00000       0.00000       0.00000       1.00000   #rsd_init#  
+past_comm                1         1  
+                                        past             y       0.00000   20000.00000       0.00000       0.00000       1.00000   #rsd_init#  
+tuhb_comm                1         1  
+                                        tuhb             n       0.00000       0.00000       0.00000       0.00000       0.00000   #rsd_init#  
+frst_comm                1         1  
+                                        frst             y       2.00000   50000.00000       0.00000       0.00000       1.00000   #rsd_init# 
+wetw_comm                1         1  
+                                        wetw             y       2.00000   50000.00000       0.00000       0.00000       1.00000   #rsd_init# 
+wewo_comm                1         1  
+                                        wewo             y       2.00000   50000.00000       0.00000       0.00000       1.00000   #rsd_init# 
+wehb_comm                1         1  
+                                        wehb             y       2.00000   50000.00000       0.00000       0.00000       1.00000   #rsd_init# 
